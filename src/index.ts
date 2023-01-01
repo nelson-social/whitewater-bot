66268d3661867e1e1c97ed05feae8100efd9e6bb
Keith Grennan <keith@fable.co>
2023-01-01 07:28:54 -0800

    exit code 1 on error

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import { postWeatherStatus } from './bot.js';
 
 postWeatherStatus()
   .then((status) => console.log(`Posted ${status.url}: ${status.content}`))
-  .catch((e) => console.error(e));
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
